1fe7053e21e4cb4e30d3723bf3fefac0b23bce8d
Megan Evans (Local) <evansm@tplxlap224.nat.physics.ox.ac.uk>
2025-04-14 20:58:09 +0100

    Tidied up code

--- space_shooter/code/main.py
+++ space_shooter/code/main.py
@@ -5,17 +5,19 @@ import pygame
 from os.path import join #To ensure path not OS dependent
 from random import randint #For star placement
 
+#General purpose Initialisation
 pygame.init() #Always initialise the module
+clock = pygame.time.Clock()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 620 
 display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption("Space Shooter")
-
 running = True
 
 #Import player   
 player_surf = pygame.image.load(join('..','images','player.png')).convert_alpha() #To ensure robustness
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
-direction = +1
+PLAYER_SPEED = 0.4
+player_direction = +1
 
 #Import star
 star_surf = pygame.image.load(join('..','images','star.png')).convert_alpha()
@@ -28,10 +30,11 @@ meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
 
 #Import laser
 laser_surf = pygame.image.load(join('..','images','laser.png')).convert_alpha()
-laser_rect = laser_surf.get_frect(bottomleft = (WINDOW_WIDTH-20, WINDOW_HEIGHT-20))
+laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT-20))
 
 
 while running:
+    clock.tick(60)
     #Event loop 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -54,11 +57,10 @@ while running:
     display_surface.blit(laser_surf, laser_rect)
 
     #Draw and move player
-    if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
-        direction = direction * -1
-    player_rect.left += direction * 0.2
-
     display_surface.blit(player_surf,player_rect)
+    player_rect.left += player_direction * PLAYER_SPEED
+    if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
+        player_direction = player_direction * -1
     
     pygame.display.update()
 
