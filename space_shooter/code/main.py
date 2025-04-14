263339d527b0247d35588c1fd81d82838d414871
Megan Evans (Local) <evansm@tplxlap224.nat.physics.ox.ac.uk>
2025-04-14 20:07:42 +0100

    Added randomly placed stars

--- space_shooter/code/main.py
+++ space_shooter/code/main.py
@@ -1,6 +1,7 @@
 #When you've written more stuff, it's a good idea to put a header here explaining what things do
 import pygame #It is always a good idea to check that your imports work!
 from os.path import join #To ensure path not OS dependent
+from random import randint
 
 pygame.init() #Always initialise the module
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 620 
@@ -9,20 +10,34 @@ pygame.display.set_caption("Space Shooter")
 
 running = True
 
-#Importing an image   
+#Import player   
 player_surf = pygame.image.load(join('..','images','player.png')).convert_alpha() #To ensure robustness
 x = 100
+
+#Import star
+star_surf = pygame.image.load(join('..','images','star.png')).convert_alpha()
+#Make list of 20 random star positions
+star_pos = [(randint(0,WINDOW_WIDTH),randint(0,WINDOW_HEIGHT)) for i in range(20)]
+
 while running:
-    x += 0.1
     #Event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             break
-
+ 
     #Draw loop
+    #Fill surface
     display_surface.fill('darkgray')
+    
+    #Move and draw player
+    x += 0.1
     display_surface.blit(player_surf,(x,100))
+    
+    #Randomly position and draw stars
+    for i in range(20):
+        display_surface.blit(star_surf,star_pos[i])
+
     pygame.display.update()
 
 pygame.quit() 
