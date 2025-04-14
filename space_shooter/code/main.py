9392a6bc84236d2a102d4c398c8cc6bfc8775b77
Megan Evans (Local) <evansm@tplxlap224.nat.physics.ox.ac.uk>
2025-04-14 19:56:53 +0100

    Added ship

--- space_shooter/code/main.py
+++ space_shooter/code/main.py
@@ -1,5 +1,7 @@
 #When you've written more stuff, it's a good idea to put a header here explaining what things do
 import pygame #It is always a good idea to check that your imports work!
+from os.path import join #To ensure path not OS dependent
+
 pygame.init() #Always initialise the module
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 620 
 display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
@@ -7,8 +9,11 @@ pygame.display.set_caption("Space Shooter")
 
 running = True
 
-
+#Importing an image   
+player_surf = pygame.image.load(join('..','images','player.png')).convert_alpha() #To ensure robustness
+x = 100
 while running:
+    x += 0.1
     #Event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -16,7 +21,8 @@ while running:
             break
 
     #Draw loop
-    display_surface.fill('blue')
+    display_surface.fill('darkgray')
+    display_surface.blit(player_surf,(x,100))
     pygame.display.update()
 
 pygame.quit() 
